f1a83941fb71baf35059915399b97756a7bbadad
jschwien <31824067+jschwien@users.noreply.github.com>
2017-09-29 21:34:47 -0400

    Update homework.lisp

--- homework.lisp
+++ homework.lisp
@@ -1,3 +1,9 @@
+
+;;Joseph Schwien
+;;CSC 345
+;;Professor Wyatt
+
+
 (defun my-replace (e1 e2 L)
   "function will deep replace a member in the list that is eqqual to e1 and replace it with e2"
   (cond ((endp L) nil)
@@ -5,6 +11,7 @@
 	  ;;if first/current member of the list is equal to e1 replace it with e2 put it back with rest of list
 	((equal (first L) e1)(cons e2 (my-replace e1 e2 (rest L))))
 	  
+	  ;; if a member of the list is a list it self we cons whats on the list with the current list
 	((listp (first L)) (cons (my-replace e1 e2 (first L)) (my-replace e1 e2 (rest L))))
 	  
 	  ;;if the current value is not equal to e1 and not a list cons the rest of the list 
@@ -13,22 +20,30 @@
 
 
 (defun fibonacci (n)
+	"this function will use the text book use of double recursion to do fibonacci sequence"
+	
+	;;checks for base case if 1 return 0 or if 2 return 1
   (cond ((eql n 1)0)
 	((eql n 2)1)
 
+	  ;;if the function is not done evaluating it will recusivley call on itself until it reaches base case
 	((+ (fibonacci(- n 1)) (fibonacci(- n 2))))))
 
 (defun fibonacci-TR (n)
-
+	
+"this function will use tail recursion to evaluate the fibonacci sequence"
+	
+;;have auxilary function that will hold accumulator values and current value
   (labels((fibonacci-extra (oneacc twoacc current)
 
-
+		;; checks for base case first, if current value is larger then 3 we ad accumulators 
 	    (cond((eql current 1) 0)
 		 ((eql current 2) 1)
 		 ((eql current 3)(+ oneacc twoacc))
 
+		   ;;Will recursively call itself until ir reaches its base case
 		 ((fibonacci-extra (+ oneacc twoacc) oneacc (- current 1))))))
-
+;;call au
 (fibonacci-extra 1 0 n)))
 
 
